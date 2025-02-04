be8bb3168096ec070db0b134d22dcc38f7ca2862
Reno Susanto <renos8157@gmail.com>
2025-02-04 08:45:26 +0700

    ini pertama

---
+++ database/migrations/2025_02_03_055834_create_tanggapans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tanggapans', function (Blueprint $table) {
+            $table->id('id_tanggapan');
+            $table->unsignedBigInteger('id_pengaduan');
+            $table->foreign('id_pengaduan')->references('id_pengaduan')->on('pengaduans')->onDelete('cascade');
+            $table->text('tanggapan');
+            $table->unsignedBigInteger('id_petugas');
+            $table->foreign('id_petugas')->references('id_petugas')->on('petugas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tanggapans');
+    }
+};
